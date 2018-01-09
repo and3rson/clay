e58101392023816b377006c63f8758f26a914148
Andrew Dunai <andunai@gmail.com>
2018-01-09 12:02:44 +0200

    Update app.py

--- clay/app.py
+++ clay/app.py
@@ -78,7 +78,7 @@ class AppWidget(urwid.Frame):
             """
             self.set_text(
                 [
-                    ('panel_divider_focus' if active else 'panel_divider', '\u23b8 '),
+                    ('panel_divider_focus' if active else 'panel_divider', u'\u23b8 '),
                     ('panel_focus' if active else 'panel', self.get_title() + '  ')
                 ]
             )
